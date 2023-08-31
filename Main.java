207fd92631a990cfbc95f3ef514c5b052ef1556f
Arthur-H15 <81658735+Arthur-H15@users.noreply.github.com>
2023-08-30 22:58:34 -0300

    criado listagem de usuarios e verificacao de conta cadastrada

--- Main.java
+++ Main.java
@@ -1,45 +1,39 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import contaTerminal.Exception.ParametroInvalido;
 import contaTerminal.clientes.Clientes;
 
-
-
-
-
 public class Main {
-    private static Scanner scanner=new Scanner(System.in);
+    private static Scanner scanner = new Scanner(System.in);
     private static ArrayList<Clientes> bancoClientes = new ArrayList<>();
-    
-    
-
 
     public static void main(String[] args) {
-        System.out.println("hello World");
         try {
-            
+
             Menu();
-           
+
             // Scanner input = new Scanner(System.in);
             // System.out.println("insira Seu Saldo:");
             // double saldo = input.nextDouble();
-            // clientes cliente = new clientes("arthur  henrique ferreira silva", "001-0", "0010", saldo);
+            // clientes cliente = new clientes("arthur henrique ferreira silva", "001-0",
+            // "0010", saldo);
             // System.out.println("nome=" + cliente.getAgencia());
             // input.close();
         } catch (Exception e) {
             // TODO: handle exception
             System.out.println("Error=>" + e);
-        }
-        finally{
+        } finally {
             scanner.close();
         }
 
     }
 
-    
     public static void Menu() {
-       
+
         int escolha = -1;
 
         while (escolha != 0) {
@@ -53,7 +47,7 @@ public class Main {
                         CriarCliente();
                         break;
                     case 2:
-                        System.out.println("Você escolheu a opção 2.");
+                        listarClientes();
                         break;
                     case 3:
                         System.out.println("Você escolheu a opção 3.");
@@ -70,45 +64,72 @@ public class Main {
             }
         }
 
-        
     }
 
     public static void exibirMenu() {
         System.out.println("==== Menu ====");
         System.out.println("1 adicionar Cliente ");
-        System.out.println("2. Opção 2");
-        System.out.println("3. Opção 3");
+        System.out.println("2.clientes cadastrados");
         System.out.println("0. Encerrar");
         System.out.print("Escolha uma opção: ");
     }
+
     public static void CriarCliente() {
         try {
-            
-        
-        // private String nome;
-        // private String agencia;
-        // private String numero;
-        // private double saldo;
-        System.out.println("Digite Nome Do Cliente:");
-        String nome = scanner.next();
-        System.out.println("Digite Agencia:");
-        String agencia = scanner.next();
-        System.out.println("Digite Numero Da Conta:");
-        String conta = scanner.next();
-        System.out.println("digite o saldo:");
-        double saldo =scanner.nextDouble();
-     Clientes cliente=   new Clientes(nome, agencia, conta, saldo);
-      boolean adicionado=bancoClientes.add(cliente);
-    //  String mensagem = String.format("cliente s% criado Com sucesso", cliente.getNome(), cliente.getAgencia(), cliente.getNumero(), cliente.getSaldo());
-    //  System.out.println(mensagem);
-     System.out.println("bancoClientes =>");
-     System.out.println(bancoClientes.toArray());
-    } catch (Exception e) {
-        System.out.println(e);
+
+            // private String nome;
+            // private String agencia;
+            // private String numero;
+            // private double saldo;
+            System.out.println("Digite Nome Do Cliente:");
+            String nome = scanner.next();
+            System.out.println("Digite Agencia:");
+            String agencia = scanner.next();
+            System.out.println("Digite Numero Da Conta:");
+            String conta = scanner.next();
+            System.out.println("digite o saldo:");
+            double saldo = scanner.nextDouble();
+            Clientes cliente = new Clientes(nome, agencia, conta, saldo);
+            if(!bancoClientes.stream().filter(m-> m.getNumero()==cliente.getNumero()).findFirst().isEmpty()){
+                throw new ParametroInvalido("conta ja cadastrada");
+            };
+            boolean adicionado = bancoClientes.add(cliente);
+            if (!adicionado)
+                throw new ParametroInvalido("error ao criar usuario");
+
+            String mensagem = String.format(
+                    "Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %s e seu saldo R$ %.2f já está disponível para saque.",
+                    cliente.getNome(), cliente.getAgencia(), cliente.getNumero(), cliente.getSaldo());
+            System.out.println(
+                    "======================================================////////=============================================================");
+
+            System.out.println(mensagem);
+            System.out.println(
+                    "======================================================////////=============================================================");
+        } catch (Exception e) {
+            System.out.println(e);
             // TODO: handle exception
         }
 
-
     }
-    
+
+    public static void listarClientes() {
+        System.out.println(
+                    "======================================================////////=============================================================");
+        if (bancoClientes.size() <= 0) {
+          
+            System.out.println("nenhum cliente cadastrado!!!!");
+          
+        }
+        else{
+            System.out.println("Clientes:");
+            for (int i = 0; i < bancoClientes.size(); i++) {
+                Clientes cliente=bancoClientes.get(i);
+                System.out.println(i+1+". : " + cliente.getNome());
+                
+            }
+        }
+        System.out.println(
+                "======================================================////////=============================================================");
+    }
 }
